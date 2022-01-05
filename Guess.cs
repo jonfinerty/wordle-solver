aa37b69eee55813abc71d5b34b7f739b3fb68a62
Jon Finerty <jonathanfinerty@gmail.com>
2022-01-05 08:27:53 +0000

    Initial code dump �

---
+++ Guess.cs
@@ -0,0 +1,114 @@
+using System.Text;
+using System.Runtime.CompilerServices; 
+
+public class Guess {
+    private static string validCharacters = "abcdefghijklmnopqrstuvwxyz";
+    public string word;
+    public char[] KnownLetters;
+    public int[] KnownLetterCounts = new int[validCharacters.Length+1]; // +1 as the % operator meant a = 1, b=2 and this is fewer operations than correcting by 1 every array index access
+    public char[] MisplacedLetters;
+    private int[] MisplacedLetterCounts = new int[validCharacters.Length+1];
+    public char[] EliminatedLetters;
+    //public HashSet<char> EliminatedLetters = new HashSet<char>(); 
+
+    private Guess(string guessWord) {
+        word = guessWord;
+        KnownLetters = new char[word.Length];
+        MisplacedLetters = new char[word.Length];
+        EliminatedLetters = new char[word.Length];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetKnownLetterCount(char c) {
+        return KnownLetterCounts[(int) c % 32];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void IncreaseKnownLetterCount(char c) {
+        KnownLetterCounts[(int) c % 32]++;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int GetMisplacedLetterCount(char c) {
+        return MisplacedLetterCounts[(int) c % 32];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void IncreaseMisplacedLetterCount(char c) {
+        MisplacedLetterCounts[(int) c % 32]++;
+    }
+
+    public static Guess FromTarget(string guessWord, string targetWord) {
+        var guess = new Guess(guessWord);
+
+        for (var i=0; i<guessWord.Length; i++) {
+            var guessCharacter = guessWord[i];
+            var targetCharacter = targetWord[i];
+            if (guessCharacter == targetCharacter){
+                guess.KnownLetters[i] = guessCharacter;
+                guess.IncreaseKnownLetterCount(guessCharacter);
+            } 
+        }
+
+        for (var i=0; i<guessWord.Length; i++) {
+            var guessCharacter = guessWord[i];
+            var targetWordCount = targetWord.Count(c => c == guessCharacter);
+            if (targetWordCount > guess.GetKnownLetterCount(guessCharacter) + guess.GetMisplacedLetterCount(guessCharacter)) {
+                guess.MisplacedLetters[i] = guessCharacter;
+                guess.IncreaseMisplacedLetterCount(guessCharacter);
+            }
+        }
+
+        for (var i=0; i<guessWord.Length; i++) {
+            var guessCharacter = guessWord[i];
+            var letterFreq = guessWord.Count(c => c == guessCharacter);
+            if (letterFreq > guess.GetKnownLetterCount(guessCharacter) + guess.GetMisplacedLetterCount(guessCharacter)) {
+                guess.EliminatedLetters[i] = guessCharacter;
+            }
+        }
+
+        return guess;
+    }
+
+    public static Guess FromScore(string guessWord, string score) {
+        var guess = new Guess(guessWord);
+        for (int i=0; i<score.Length; i++) {
+            var scoreCharacter = score[i];
+            var guessCharacter = guessWord[i];
+            if (Char.ToUpper(scoreCharacter) == 'C'){
+                guess.KnownLetters[i] = guessCharacter;
+                guess.IncreaseKnownLetterCount(guessCharacter);
+                continue;
+            } 
+            if (Char.ToUpper(scoreCharacter) == 'M'){
+                guess.MisplacedLetters[i] = guessCharacter;
+                guess.IncreaseMisplacedLetterCount(guessCharacter);
+                continue;
+            }
+            if (Char.ToUpper(scoreCharacter) == 'W'){
+                guess.EliminatedLetters[i] = guessCharacter;
+                continue;
+            }
+        }
+        return guess;
+    }
+
+    public override string ToString()
+    {
+        var stringGuessRepresentation = new StringBuilder();
+        for (int i=0; i<word.Length; i++) {
+            if (KnownLetters[i] != '\0') {
+                stringGuessRepresentation.Append('C');
+                continue;
+            }
+            if (MisplacedLetters[i] != '\0') {
+                stringGuessRepresentation.Append('M');
+                continue;
+            }
+            stringGuessRepresentation.Append('W');
+
+        }
+        return $"{word} ({stringGuessRepresentation.ToString()})";
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections.Concurrent;
+
+Console.WriteLine();
+var validGuessingWords = ReadFile("wordlist_guesses2.txt");
+Console.WriteLine($"Number of valid guessing words = {validGuessingWords.Count}");
+
+var validSolutionWords = ReadFile("wordlist_solutions2.txt");
+Console.WriteLine($"Number of valid solution words = {validSolutionWords.Count}");
+
+Console.WriteLine();
+// Console.WriteLine("Enter word length");
+// var input = Console.ReadLine();
+// if (input == null) {
+//     return;
+// }
+// var wordLength = Int32.Parse(input);
+//var sanitisedWords = SanitiseWords(allGuesses, wordLength);
+//Console.WriteLine($"Number of {wordLength} letter words = {sanitisedWords.Count}");
+var remainingViableWords = validSolutionWords;
+Console.WriteLine($"Remaining possibilities: {remainingViableWords.Count}");
+
+// var timer = System.Diagnostics.Stopwatch.StartNew();
+// var firstGuessWords = FindOptimalGuessChoices(validGuessingWords, remainingViableWords);
+// if (firstGuessWords.Count > 1) {
+//     firstGuessWords = firstGuessWords.Where(w => remainingViableWords.Contains(w)).ToList();
+// }
+// timer.Stop();
+// var elapsedTime = timer.ElapsedMilliseconds;
+// Console.WriteLine($"Suggested word choices: {string.Join(',',firstGuessWords)}, determined in {elapsedTime}ms");
+// return;
+
+while(true) {
+    Console.WriteLine("Enter guess");
+    var input = Console.ReadLine();
+    if (input == null) {
+        return;
+    }
+    Console.WriteLine("Enter score, C=correct, M=misplaced, W=wrong");
+    var manualScore = Console.ReadLine();
+    if (manualScore == null) {
+        return;
+    }
+    var guess = Guess.FromScore(input.ToLower(), manualScore);
+
+    remainingViableWords = computeRemainingPossibleAnswers(guess, remainingViableWords);
+    Console.WriteLine($"Remaining possibilities: {remainingViableWords.Count}");
+    if (remainingViableWords.Count == 0) {
+        Console.WriteLine("Looks like the word you're searching for isn't in the target dictionary, :(");
+        return;
+    }
+    
+    if (remainingViableWords.Count == 1) {
+        Console.WriteLine($"Only one possibility remains, and so the answer is {remainingViableWords.First()}!");
+        return;
+    }
+
+    if (remainingViableWords.Count < 20) {
+        Console.WriteLine(string.Join(',', remainingViableWords));
+    }
+
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    var recommendedWords = FindOptimalGuessChoices(validGuessingWords, remainingViableWords);
+    var recommendedWordsInAnswerSet = recommendedWords.Where(w => remainingViableWords.Contains(w)).ToList();
+    //Console.WriteLine($"Unfiltered suggested word choices: {string.Join(',', recommendedWords)}");
+    watch.Stop();
+    var elapsedMs = watch.ElapsedMilliseconds;
+    if (recommendedWordsInAnswerSet.Count > 0) {
+        Console.WriteLine($"Suggested word choices: {string.Join(',',recommendedWordsInAnswerSet)}, determined in {elapsedMs}ms");
+    } else {
+        Console.WriteLine($"Suggested word choices: {string.Join(',',recommendedWords)}, determined in {elapsedMs}ms");
+    }
+}
+
+static List<string> ReadFile(string fileName) {
+    var lines = File.ReadAllLines(fileName);
+    return lines.ToList();
+}
+
+static List<string> SanitiseWords(List<string> wordlist, int wordLength) {
+    return wordlist
+    .Where(w => w.Length == wordLength)
+    .Where(w => w.All(Char.IsLetter))
+    .ToList();
+}
+
+
+// for guess choice, run through every target word
+// assign the the guess choice a value based on how many words it removes from the corpus
+static List<string> FindOptimalGuessChoices(List<string> wordlist, List<string> remainingViableWords) {
+
+    var highWatermarkValue = int.MaxValue;
+    var results = new ConcurrentBag<(string, int)>();
+    Parallel.ForEach(wordlist, new ParallelOptions {MaxDegreeOfParallelism = 12}, possibleGuessWord => {
+    // foreach (var possibleGuessWord in wordlist)
+    // {
+        //Console.WriteLine($"Guess word = {possibleGuessWord}");
+        var totalPossibilities = 0;
+        foreach (var possibleTargetWord in remainingViableWords) {
+            //Console.WriteLine($"Possible target word = {possibleTargetWord}");
+            var guess = Guess.FromTarget(possibleGuessWord, possibleTargetWord);
+            var remainingPossibleAnswers = computeRemainingPossibleAnswers(guess, remainingViableWords);
+            totalPossibilities += remainingPossibleAnswers.Count;
+            if (totalPossibilities > highWatermarkValue) {
+                //Console.WriteLine($"Aborting guess '{possibleGuessWord}' as it's already suboptimal");
+                goto ABORT;
+            }
+        }
+
+        if (totalPossibilities > 0){
+            //Console.WriteLine($"Guess '{possibleGuessWord}' evaluated with score '{totalPossibilities}'");
+            results.Add((possibleGuessWord, totalPossibilities));
+            if (totalPossibilities < highWatermarkValue) {
+                highWatermarkValue = totalPossibilities;
+            }
+        }
+        ABORT:;
+    });
+    //}
+
+    var currentBestGuesses = new List<string>();
+    var currentBestGuessTotalPossibilities = int.MaxValue;
+    foreach (var evalutedGuess in results)
+    {
+        if (evalutedGuess.Item2 < currentBestGuessTotalPossibilities && evalutedGuess.Item2 > 0) {
+            currentBestGuessTotalPossibilities = evalutedGuess.Item2;
+            currentBestGuesses.Clear();
+            currentBestGuesses.Add(evalutedGuess.Item1);
+            //Console.WriteLine($"New optimal guess found: {evalutedGuess.Item1}, which reduces the set of possible answers to {evalutedGuess.Item2} across all games");
+        } else if (evalutedGuess.Item2 == currentBestGuessTotalPossibilities) {
+            currentBestGuesses.Add(evalutedGuess.Item1);
+            //Console.WriteLine($"New equally optimal guess found: {evalutedGuess.Item1}, which reduces the set of possible answers to {evalutedGuess.Item2} across all games");
+        }
+    }
+
+    // foreach (var possibleGuessWord in wordlist) {
+    //     //Console.WriteLine($"Guess word = {possibleGuessWord}");
+    //     var totalPossibilities = 0;
+    //     foreach (var possibleTargetWord in remainingViableWords) {
+    //         //Console.WriteLine($"Possible target word = {possibleTargetWord}");
+    //         var guess = Guess.FromTarget(possibleGuessWord, possibleTargetWord);
+    //         var remainingPossibleAnswers = computeRemainingPossibleAnswers(guess, remainingViableWords);
+    //         totalPossibilities += remainingPossibleAnswers.Count;
+    //     }
+
+    //     if (totalPossibilities < currentBestGuessTotalPossibilities && totalPossibilities > 0) {
+    //         currentBestGuessTotalPossibilities = totalPossibilities;
+    //         currentBestGuesses.Clear();
+    //         currentBestGuesses.Add(possibleGuessWord);
+    //         Console.WriteLine($"New optimal guess found: {possibleGuessWord}, which on average reduces the set of possible answers to {totalPossibilities} across all games");
+    //     } else if (totalPossibilities == currentBestGuessTotalPossibilities) {
+    //         currentBestGuesses.Add(possibleGuessWord);
+    //         Console.WriteLine($"New equally optimal guess found: {possibleGuessWord}, which on average reduces the set of possible answers to {totalPossibilities} across all games");
+    //     }
+    // }
+
+    return currentBestGuesses;
+}
+
+static List<string> computeRemainingPossibleAnswers(Guess guess, List<string> remainingViableWords) {
+    var validWords = new List<string>(remainingViableWords.Count);
+    foreach (var word in remainingViableWords) {
+
+        bool validWord = true;
+
+        for (var i=0; i<guess.KnownLetters.Length; i++) {
+            var knownLetter = guess.KnownLetters[i];
+            if (knownLetter == '\0') {
+                continue;
+            }
+            if (knownLetter != word[i]) {
+                //Console.WriteLine($"Eliminating {word} from remainingViableWords as does not contain known letter {item.Value} at index {item.Key}");
+                validWord = false;
+                break;
+            }
+        }  
+
+        if (!validWord) {
+            continue;
+        }
+
+        // Validate misplacedLetters counts
+        for (int i=0; i<guess.MisplacedLetters.Length; i++) {
+            var letter = guess.MisplacedLetters[i];
+            var countTarget = word.Count(c => c == letter);
+            if (countTarget < guess.GetKnownLetterCount(letter) + guess.GetMisplacedLetterCount(letter)) {
+                //Console.WriteLine($"Eliminating {word} from remainingViableWords as does not contain enough misplaced letters '{letter}' ({countTarget} in word, {knownCountGuess} in known locations, {misplacedCountGuess} in unknown locations");
+                validWord = false;
+                break;
+            }
+
+            // Validate not in known bad locations
+            if (letter == word[i]) {
+                //Console.WriteLine($"Eliminating {word} from remainingViableWords as contains letter '{item.Value}' in known-misplaced position '{item.Key}'");
+                validWord = false;
+                break;
+            }
+        }
+
+        if (!validWord) {
+            continue;
+        }
+
+        foreach(var eliminatedLetter in guess.EliminatedLetters) {
+            if (eliminatedLetter == '\0') {
+                continue;
+            }
+            // if number of letter in word > known + misplaced then non viable
+            var wordOccurances = word.Count(c => c == eliminatedLetter);
+        
+            if (wordOccurances > guess.GetKnownLetterCount(eliminatedLetter) + guess.GetMisplacedLetterCount(eliminatedLetter)) {
+                //Console.WriteLine($"Eliminating {word} from remainingViableWords as contains eliminated letter '{eliminatedLetter}' ({wordOccurances} times in word, {knownOccurances} times in known locations, {misplacedOccurances} times misplaced)");
+                validWord = false;
+                break;
+            }
+        }
+
+        if (validWord) {
+            //Console.WriteLine($"Adding '{word}' as valid word for guess = {guess}");
+            validWords.Add(word);
+        }
+    }
+
+    return validWords;
+}
+
+
